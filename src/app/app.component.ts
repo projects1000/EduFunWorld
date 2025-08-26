73e36c5aeb5bbc99ec38970b7a60a6a8939185b2
Priyabrata Pattanaik <githubprojects100@gmail.com>
2025-08-26 23:46:53 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,30 @@ export class AppComponent {
   title = 'AngularApp';
   showMenu = false;
 
+  deferredPrompt: any = null;
+  showInstallButton = false;
+
+  constructor() {
+    window.addEventListener('beforeinstallprompt', (e: any) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      this.showInstallButton = true;
+    });
+  }
+
   startGame() {
     this.showMenu = true;
+  }
+
+  installPWA() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          this.showInstallButton = false;
+        }
+        this.deferredPrompt = null;
+      });
+    }
   }
 }
